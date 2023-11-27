8e0a94b623630e46e6f5e8f50b4ee0ba21d53d51
Catalina Arriagada <75139474+catalina-arriagada@users.noreply.github.com>
2023-11-27 11:01:49 -0400

    Update 05-app.js

---
+++ 10-Estructuras/js/05-app.js
@@ -0,0 +1,25 @@
+//no es necesario poner autentiado==true porque se da por hecho que usuario esta autenticado.
+const auenticado = true;
+if(autenticado) {
+  console.log('usuario autenticado);
+}
+//videojuego ej:
+const puntaje =500,
+  if(punaje > 300) {
+  console.log('felicidades beun puntaje');
+  return;
+}
+
+ const puntaje =500,
+//si le pongo un return ya no se ejecuta el resto del codigo, pero solo funciona dentro de un if dentro de una funcion.
+function revisarPuntaje(){
+ 
+    if(punaje > 300) {
+    console.log('felicidades beun puntaje');
+    return;
+    }
+    if(punaje > 300) {
+    console.log('felicidades beun puntaje');
+    return;
+  }
+}
