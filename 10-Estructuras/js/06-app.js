2491648f064c86380f4961a6a0587da21b4906fc
Catalina Arriagada <75139474+catalina-arriagada@users.noreply.github.com>
2023-11-27 11:08:16 -0400

    Update 06-app.js

---
+++ 10-Estructuras/js/06-app.js
@@ -0,0 +1,14 @@
+//operador ternario:
+cons autenticado = true; 
+cons puedePagar = true; 
+
+console.log(autenticado && puedePagar ? 'si esta autenticado' : 'no esta autenticado');
+
+//else if
+//ternarios anidados
+console.log(autenticado ? puedePagar ? 'si esta autenticado y puede' : 'si esta autenticado yno  puede' :
+            'no esta autenticado');
+
+
+
+
