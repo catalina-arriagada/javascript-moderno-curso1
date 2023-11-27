c106ddc93a51fbe3ea3324cad8d375c737b661a5
Catalina Arriagada <75139474+catalina-arriagada@users.noreply.github.com>
2023-11-27 11:17:51 -0400

    Update 01-app.js

---
+++ 11-Iteradores/js/01-app.js
@@ -0,0 +1,11 @@
+//inicializador(parte donde empieza a contar, el indice); condicion; incremento)
+for(let i = 0; i<10; i++){
+  console.log(´NUMero ${i}´);
+}
+
+//saber si numero es par (con modulo)
+for(let i=1; i<=20; i++) {
+  if(i % 2 === 0){
+    console.log('par$(i)');
+  }
+}
