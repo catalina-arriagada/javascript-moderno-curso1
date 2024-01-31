f4e7837c722dd34aa905a694e1abeee12a646324
Catalina Arriagada <catalina.arriagada1@gmail.com>
2024-01-30 23:42:27 -0300

    cap 51

--- 51-Testing-Jest/__tests__/citas.js
+++ 51-Testing-Jest/__tests__/citas.js
@@ -3,9 +3,11 @@ import Citas from '../js/classes/Citas';
 describe('probar clase Citas', () => {
     
     const citas = new Citas();
-    
+    const id = Date.now();
+
     test('test para agregar nueva cita', () => {
         const citaObj = {
+            id,
             mascota: 'Hook',
             propietario: 'j',
             telefono: '111',
@@ -13,11 +15,29 @@ describe('probar clase Citas', () => {
             hora:'10:30',
             sintomas: 'aaa'
         };
-        citaObj.id = Date.now();
         citas.agregarCita(citaObj);
 
         //Escribir prueba
         expect(citas).toMatchSnapshot(); //que clase se esta guardando correctamente
         
     })
+    test('actualizar cita', () => {
+        const citaActualizada = {
+            id,
+            mascota: 'nuevo nombre',
+            propietario: 'j',
+            telefono: '111',
+            fecha: '10-12-10',
+            hora:'10:30',
+            sintomas: 'aaa2'
+        };
+        citas.editarCita(citaActualizada);
+        expect(citas).toMatchSnapshot();
+    })
+    test('eliminar cita', () => {
+        citas.eliminarCita(id);
+        expect(citas).toMatchSnapshot();
+    })
 })
+
+//npm t -- -u
