a1e3fd3a5372ff032c01a41a36c1c14d3ec4382c
Catalina Arriagada <catalina.arriagada1@gmail.com>
2024-02-06 21:47:36 -0300

    cap 52

--- 52-Testing-Cypress/js/app.js
+++ 52-Testing-Cypress/js/app.js
@@ -1,3 +1,14 @@
+//CYPRESS:
+//-es una herramienta de nueva generacion para hacer testing
+//-cuando escribes pruebas son de codigo, pero no sabes que es lo que el usuario experimenta, pero con CYPRESS si se puede ver lo q el usuario VE
+//cypress.io
+//-Ofrede end-to-end pero tambien unitarias y de integracion
+//-hace bien como integrar un archivo con otro
+//-funciona con el navegador (c ve en el navegador), se puede probar con Cypress proyectos en Java, c#, python, php, node, react, o vuejs
+
+
+
+
 import App from './classes/App.js';
 
 const app = new App();
